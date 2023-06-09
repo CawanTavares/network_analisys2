d194aa208ad4f6e890264ae92e2871c10cf2e225
CawanTavares <99696275+CawanTavares@users.noreply.github.com>
2023-06-09 10:51:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import networkx as nx
 import matplotlib.pyplot as plt
-from PIL import Image
+import numpy as np
 
 # Criar um grafo vazio
 G = nx.karate_club_graph()
@@ -44,25 +44,25 @@ plt.figure(figsize=(8, 8))
 nx.draw_networkx(G, with_labels=True, node_color='lightblue', node_size=400, font_size=10, alpha=0.8)
 plt.axis('off')
 plt.savefig('graph.png', dpi=300)
+plt.show()
 
-# Função para exibir o grafo no Streamlit
-def show_graph():
-    image = Image.open('graph.png')
-    st.image(image, use_column_width=True)
-
-# Configuração do app Streamlit
-st.title('Análise Exploratória de Rede')
-st.subheader('Grafo')
-show_graph()
+# Configurar o streamlit para ignorar o aviso de depreciação
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Exibir outras informações da rede
+st.title('Análise Exploratória de Rede')
+st.subheader('Grafo')
+img = plt.imread('graph.png')
+st.image(img, use_column_width=True)
+
 st.subheader('Informações da Rede')
 st.write(f'Diâmetro: {diameter}')
 st.write(f'Periferia: {periphery}')
 st.write('Histograma de distribuição empírica de grau')
-plt.hist(degrees, bins=max(degrees)-min(degrees)+1, rwidth=0.8)
+plt.hist(degrees, bins=np.arange(min(degrees), max(degrees)+1), rwidth=0.8)
+plt.xlabel('Grau')
+plt.ylabel('Contagem')
 st.pyplot()
-
 st.write(f'Coeficiente de clustering global: {global_clustering}')
 st.write(f'Componentes Conectados: {connected_components}')
 st.write(f'Eigenvector centrality: {eigenvector_centrality}')
